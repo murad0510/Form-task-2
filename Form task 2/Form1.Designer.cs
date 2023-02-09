35b6184761818770b8d577c60547d22109ab96dc
user <user@WIN-47DPTRABTUT>
2023-02-09 19:57:33 +0400

    Add project files.

---
+++ Form task 2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Form_task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.MaskedTextBox();
+            this.Email = new System.Windows.Forms.MaskedTextBox();
+            this.Surname = new System.Windows.Forms.MaskedTextBox();
+            this.PhoneNumber = new System.Windows.Forms.MaskedTextBox();
+            this.Age = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Birthday = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(59, 114);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(197, 22);
+            this.Name.TabIndex = 0;
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(403, 114);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(233, 22);
+            this.Email.TabIndex = 1;
+            // 
+            // Surname
+            // 
+            this.Surname.Location = new System.Drawing.Point(59, 215);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(197, 22);
+            this.Surname.TabIndex = 2;
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.Location = new System.Drawing.Point(403, 215);
+            this.PhoneNumber.Mask = "(994)-00-000-00-00";
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.Size = new System.Drawing.Size(125, 22);
+            this.PhoneNumber.TabIndex = 3;
+            // 
+            // Age
+            // 
+            this.Age.Location = new System.Drawing.Point(59, 310);
+            this.Age.Mask = "00000";
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(44, 22);
+            this.Age.TabIndex = 4;
+            this.Age.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Form";
+            // 
+            // Birthday
+            // 
+            this.Birthday.Location = new System.Drawing.Point(233, 310);
+            this.Birthday.Name = "Birthday";
+            this.Birthday.Size = new System.Drawing.Size(246, 22);
+            this.Birthday.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(564, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 95);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Log in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(400, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(400, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Phone number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(230, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Birthday";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Surname";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 288);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Age";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Birthday);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.PhoneNumber);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Name);
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox Name;
+        private System.Windows.Forms.MaskedTextBox Email;
+        private System.Windows.Forms.MaskedTextBox Surname;
+        private System.Windows.Forms.MaskedTextBox PhoneNumber;
+        private System.Windows.Forms.MaskedTextBox Age;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker Birthday;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form task 2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_task_2
+{
+    public partial class Form1 : Form
+    {
+        public class FileHelper
+        {
+            public static void WriteJsonHuman(Human human)
+            {
+                var serializer = new JsonSerializer();
+                using (var sw = new StreamWriter($"{human.Name}.json"))
+                {
+                    using (var jw = new JsonTextWriter(sw))
+                    {
+                        jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                        serializer.Serialize(jw, human);
+                    }
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                                                                  Color.FromArgb(209, 210, 249),
+                                                                  Color.FromArgb(169, 188, 208),
+                                                                  90F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < PhoneNumber.Text.Length; i++)
+            {
+
+            }
+            if (Name.Text != null && Surname.Text != null && Age.Text != null && Email.Text.EndsWith("@gmail.com") && PhoneNumber.Text.Length == 18 && Birthday.Text != null)
+            {
+                Human humans = new Human()
+                {
+                    Name = Name.Text,
+                    Surname = Surname.Text,
+                    Age = int.Parse(Age.Text),
+                    Email = Email.Text,
+                    Phone = PhoneNumber.Text,
+                    BirthDate = Birthday.Text.ToString(),
+                };
+
+                FileHelper.WriteJsonHuman(humans);
+                MessageBox.Show("The operation was successfully performed.");
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("There may be information that you did not mention.");
+            }
+        }
+    }
+}
+public class Human
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string BirthDate { get; set; }
+}
